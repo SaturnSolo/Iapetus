d9d704c8efa99b467ccf199249d804ce0d612ba8
Vulpic <66442608+vulpic@users.noreply.github.com>
2023-10-04 13:16:08 -0500

    Update AdventureButtons.java

--- src/main/java/org/example/buttons/AdventureButtons.java
+++ src/main/java/org/example/buttons/AdventureButtons.java
@@ -3,9 +3,18 @@ package org.example.buttons;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.example.ButtonManager;
+import org.example.Main;
 import org.example.structures.IapetusButton;
 
 public class AdventureButtons {
+    ButtonManager bm = Main.buttonManager;
+    public AdventureButtons() {
+        bm.addButtons(
+          new ConfirmButton()
+        );
+    }
+
     private static String getRandom() {
         return "random-event";
     }
